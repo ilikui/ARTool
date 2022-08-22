faea3be999603368ac3ddc1f7ebf0f954fafd349
= <kui2.li@aptiv.com>
2022-08-22 13:05:47 +0800

    update by likui

--- mscript/ReadExcel.m
+++ mscript/ReadExcel.m
@@ -1,6 +1,6 @@
 %  Operate Excel
 %
-% 
+%
 
 ExcelFile = "\LockgMgr.xlsx";
 
@@ -33,9 +33,21 @@ end
 wdata = Open(WorkBook,ExcelFilePath);
 
 sheets = wdata.Sheets;
+
+sheetName ={};
+
+% for i=1:sheets.Count
+%     
+%      Obh=Item(sheets,i);
+% end
+%wes=sheets.Item(1);
+
+
+
+
 sheet12 = Item(sheets,2);
 range = get(sheet12,'Range','D1','F6');
 range.value
 wdata.Close();
 ExcelApp.Quit();
-    
+
